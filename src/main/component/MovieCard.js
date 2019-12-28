abb29789ae4ef3a795f7a9198898d2c163e1c2b8
Mohammad_Faizan <mohammad_faizan@external.mckinsey.com>
2019-12-28 22:22:44 +0530

    adding router

--- src/main/component/MovieCard.js
+++ src/main/component/MovieCard.js
@@ -2,6 +2,31 @@ import React, {Component} from 'react';
 
 import {Card, Divider, Image, Rating} from 'semantic-ui-react'
 import VideoPopup from "./VideoPopup";
+import Carousel from "react-multi-carousel";
+import Container from "react-bootstrap/Container";
+import "react-multi-carousel/lib/styles.css";
+import FavRated from "./FavRated";
+
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: {max: 4000, min: 3000},
+        items: 5,
+    },
+    desktop: {
+        breakpoint: {max: 3000, min: 1024},
+        items: 5,
+        partialVisibilityGutter: 40
+    },
+    tablet: {
+        breakpoint: {max: 1024, min: 464},
+        items: 5,
+    },
+    mobile: {
+        breakpoint: {max: 464, min: 0},
+        items: 3,
+    },
+};
 
 class MovieCard extends Component {
 
@@ -18,114 +43,212 @@ class MovieCard extends Component {
 
     render() {
         return (
-
-            <Container fluid={true}><br/>
+            <Container fluid={true} className="scrolling-wrapper"><br/>
                 <Divider horizontal><h2>Action</h2></Divider>
-                <Row noGutters={true}>
-                    <Col>
-                    <Card>
-                        <Image src={require('./movie.jpeg')} wrapped ui={false}/>
-                        <Card.Content>
-                            <Card.Header>{this.state.name}</Card.Header>
-                            <Card.Meta>
-                                <span className='date'>{this.state.releaseDate}</span>
-                            </Card.Meta>
-                            <Card.Description>
-                                {this.state.info}
-                            </Card.Description><Card.Description>
-                            <VideoPopup/>
+                <Carousel responsive={responsive}>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="150"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <VideoPopup/>
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            <FavRated fav = {1}/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+
+
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="150"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
                             </Card.Description>
 
-                        </Card.Content>
-                        <Card.Content extra>
-                            <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
-                        </Card.Content>
-                    </Card>
-                    </Col><Col>
-                    <Card>
-                        <Image src={require('./movie.jpeg')} wrapped ui={false}/>
-                        <Card.Content>
-                            <Card.Header>{this.state.name}</Card.Header>
-                            <Card.Meta>
-                                <span className='date'>{this.state.releaseDate}</span>
-                            </Card.Meta>
-                            <Card.Description>
-                                {this.state.info}
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="150"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
                             </Card.Description>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
-                        </Card.Content>
-                    </Card>
-                    </Col><Col>
-                    <Card>
-                        <Image src={require('./movie.jpeg')} wrapped ui={false}/>
-                        <Card.Content>
-                            <Card.Header>{this.state.name}</Card.Header>
-                            <Card.Meta>
-                                <span className='date'>{this.state.releaseDate}</span>
-                            </Card.Meta>
-                            <Card.Description>
-                                {this.state.info}
+
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="150"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
                             </Card.Description>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
-                        </Card.Content>
-                    </Card>
-                    </Col><Col>
-                    <Card>
-                        <Image src={require('./movie.jpeg')} wrapped ui={false}/>
-                        <Card.Content>
-                            <Card.Header>{this.state.name}</Card.Header>
-                            <Card.Meta>
-                                <span className='date'>{this.state.releaseDate}</span>
-                            </Card.Meta>
-                            <Card.Description>
-                                {this.state.info}
+
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="150"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
                             </Card.Description>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
-                        </Card.Content>
-                    </Card>
-                    </Col><Col>
-                    <Card>
-                        <Image src={require('./movie.jpeg')} wrapped ui={false}/>
-                        <Card.Content>
-                            <Card.Header>{this.state.name}</Card.Header>
-                            <Card.Meta>
-                                <span className='date'>{this.state.releaseDate}</span>
-                            </Card.Meta>
-                            <Card.Description>
-                                {this.state.info}
+
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="150"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
                             </Card.Description>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
-                        </Card.Content>
-                    </Card>
-                    </Col>
-                    <Col>
-                    <Card>
-                        <Image src={require('./movie.jpeg')} wrapped ui={false}/>
-                        <Card.Content>
-                            <Card.Header>{this.state.name}</Card.Header>
-                            <Card.Meta>
-                                <span className='date'>{this.state.releaseDate}</span>
-                            </Card.Meta>
-                            <Card.Description>
-                                {this.state.info}
+
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="250"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
                             </Card.Description>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
-                        </Card.Content>
-                    </Card>
-                    </Col>
-                </Row>
-                <hr/>
+
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="250"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
+                            </Card.Description>
+
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+                    <div>
+
+                        <Card>
+                            <Image src={require('./movie.jpeg')} width="300" height="250"/>
+                            <Card.Content>
+                                <Card.Header>{this.state.name}</Card.Header>
+                                <Card.Meta>
+                                    <span className='date'>{this.state.releaseDate}</span>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {this.state.info}
+                                </Card.Description><Card.Description>
+                                <VideoPopup/>
+                            </Card.Description>
+
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Rating defaultRating={this.state.rating} maxRating={10} disabled/>
+                            </Card.Content>
+                        </Card>
+
+                    </div>
+
+
+                    <hr/>
+                </Carousel>
             </Container>
+
+
         );
     }
 }
