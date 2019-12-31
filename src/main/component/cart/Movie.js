302a7798c75ec867be7656d84a2bc2a66ab4e399
Mohammad_Faizan <mohammad_faizan@external.mckinsey.com>
2020-01-01 00:29:15 +0530

    added fully functional admin page

--- src/main/component/cart/Movie.js
+++ src/main/component/cart/Movie.js
@@ -6,13 +6,9 @@ import Container from "@material-ui/core/Container";
 import MaterialUIForm from 'react-material-ui-form'
 import FormControl from "@material-ui/core/FormControl";
 import Button from "@material-ui/core/Button";
-import {Title} from "@material-ui/icons";
-import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
-import Header from "../../header/Header";
 import Navbar from "react-bootstrap/Navbar";
-import {Sticky} from "semantic-ui-react";
 
 
 class Movie extends Component {
@@ -23,9 +19,109 @@ class Movie extends Component {
             movies: [],
             error: [],
             selectedMovieID: '',
-            rating: 0,
-            moviesFetchedByID: []
+            movieType: '',
+            moviesFetchedByID: [],
+            rating: ''
         }
+    }
+
+
+    render() {
+
+        const {movies} = this.state
+        return (
+            <div>
+
+                <Navbar bg="dark" expand="lg" variant="dark">
+                    <Navbar.Brand href="/">
+                        {<img src={require("./M2N-logos_black.png")} alt="logo" width="100" height="50"/>}
+                    </Navbar.Brand>
+                    <Navbar.Text>
+                        <h3>Admin Page</h3>
+                    </Navbar.Text>
+                </Navbar>
+
+                <br/>
+
+                <MaterialUIForm onSubmit={this.printAll}>
+                    <Container>
+                        <FormControl aria-live={"assertive"} variant="filled">
+                            {/*{movies.length ? movies.map(r=>{t = r.Title}) : error}*/}
+                            <Autocomplete
+                                id="combo-box-demo"
+                                options={movies}
+                                getOptionLabel={option => option.Title + "-" + option.imdbID}
+                                filterSelectedOptions={true}
+                                style={{width: 400}}
+                                renderInput={params => (
+                                    <TextField
+                                        {...params}
+                                        label="Find movies" variant="outlined" fullWidth
+                                        onChange={this.handleChange}
+                                        onBlur={this.printState}
+                                    />
+                                )}
+                            >
+                            </Autocomplete>
+
+                            <br/>
+
+                            <Select
+                                labelId="demo-simple-select-filled-label"
+                                id="demo-simple-select-filled"
+                                value={this.state.movieType}
+                                name="Movie Type"
+                                onChange={this.handleMovieType}
+                            >
+
+                                <MenuItem disabled={true}><em>Movie Types</em></MenuItem>
+                                <MenuItem value={1}>Time Travel</MenuItem>
+                                <MenuItem value={2}>Time Loop</MenuItem>
+                                <MenuItem value={3}>Marvel</MenuItem>
+                                <MenuItem value={4}>DC</MenuItem>
+                                <MenuItem value={5}>Mind Bending</MenuItem>
+                                <MenuItem value={6}>Christopher Nolan</MenuItem>
+                                <MenuItem value={7}>Parallel Universe</MenuItem>
+                                <MenuItem value={8}>Mystery</MenuItem>
+                                <MenuItem value={9}>Deja Vu</MenuItem>
+                                <MenuItem value={10}>Psychological Thrillers</MenuItem>
+                            </Select>
+
+                            <br/>
+                            <Select
+                                labelId="demo-simple-select-filled-label"
+                                id="demo-simple-select-filled"
+                                value={this.state.rating}
+                                name="Movie Type"
+                                onChange={this.handleRating}>
+
+                                <MenuItem value="" disabled={true}><em>Movie Rating</em></MenuItem>
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={2}>2</MenuItem>
+                                <MenuItem value={3}>3</MenuItem>
+                                <MenuItem value={4}>4</MenuItem>
+                                <MenuItem value={5}>5</MenuItem>
+                                <MenuItem value={6}>6</MenuItem>
+                                <MenuItem value={7}>7</MenuItem>
+                                <MenuItem value={8}>8</MenuItem>
+                                <MenuItem value={9}>9</MenuItem>
+                                <MenuItem value={10}>10</MenuItem>
+                            </Select>
+
+
+                            <br/>
+
+                            <Button variant="contained" type="submit" onClick={this.clear}>Submit</Button>
+                            <br/>
+                            <Button variant="contained" type="Reset" onClick={this.clear}>Reset</Button>
+                        </FormControl>
+                    </Container>
+
+
+                </MaterialUIForm>
+
+            </div>
+        );
     }
 
     handleChange = (e) => {
@@ -53,65 +149,6 @@ class Movie extends Component {
 
     }
 
-
-    render() {
-
-        const {movies} = this.state
-        return (
-            <div>
-
-                <Navbar bg="dark" expand="lg" variant="dark">
-                    <Navbar.Brand href="/">
-                        {<img src={require("./M2N-logos_black.png")} alt="logo" width="100" height="50"/>}
-                    </Navbar.Brand>
-                </Navbar>
-
-                <MaterialUIForm onSubmit={this.printAll}>
-                    <FormControl>
-                        <Container>
-                            {/*{movies.length ? movies.map(r=>{t = r.Title}) : error}*/}
-                            <Autocomplete
-                                id="combo-box-demo"
-                                options={movies}
-                                getOptionLabel={option => option.Title + "-" + option.imdbID}
-                                filterSelectedOptions={true}
-                                style={{width: 300}}
-                                renderInput={params => (
-                                    <TextField
-                                        {...params}
-                                        label="Combo box" variant="outlined" fullWidth
-                                        onChange={this.handleChange}
-                                        onBlur={this.printState}
-                                    />
-                                )}
-                            >
-                            </Autocomplete>
-
-                            <InputLabel>Rating</InputLabel>
-                            <Select value={this.state.rating} name="Rating" onChange={this.getRating}>
-                                <MenuItem value=""><em>Please select Rating ...</em></MenuItem>
-                                <MenuItem value={1}>1</MenuItem>
-                                <MenuItem value={2}>2</MenuItem>
-                                <MenuItem value={3}>3</MenuItem>
-                                <MenuItem value={4}>4</MenuItem>
-                                <MenuItem value={5}>5</MenuItem>
-                                <MenuItem value={6}>6</MenuItem>
-                                <MenuItem value={7}>7</MenuItem>
-                                <MenuItem value={8}>8</MenuItem>
-                                <MenuItem value={9}>9</MenuItem>
-                                <MenuItem value={10}>10</MenuItem>
-                            </Select>
-                            <Button variant="" type="submit">Submit</Button>
-                        </Container>
-
-                    </FormControl>
-
-                </MaterialUIForm>
-
-            </div>
-        );
-    }
-
     printState = (e) => {
         this.setState({
             selectedMovieID: e.target.value.split('-')[1]
@@ -119,10 +156,28 @@ class Movie extends Component {
 
     }
 
-    getRating = (e) => {
+    handleMovieType = (e) => {
         this.setState(
             {
-                [e.target.name]: e.target.value
+                movieType: e.target.value
+            }
+        )
+
+    }
+
+
+    handleRating = (e) => {
+        this.setState({
+            rating: e.target.value
+        })
+    }
+
+    clear = (e) => {
+        this.setState(
+            {
+                movieType: '',
+                selectedMovieID: '',
+                moviesFetchedByID: []
             }
         )
 
@@ -143,6 +198,8 @@ class Movie extends Component {
                     {
 
                         'imdbData': res,
+                        'rating': this.state.rating,
+                        'type': this.state.movieType
                     },
                     {
                         "Access-Control-Allow-Origin": "*",
@@ -160,11 +217,8 @@ class Movie extends Component {
             .catch(val => {
                 console.log(val)
             })
-
-
         console.log('Request body', this.state.moviesFetchedByID)
         console.log('Request body', movie)
-
 
     }
 
